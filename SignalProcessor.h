010d885495f56bed2f98499d12b810351a43b615
Roger Herikstad <roger.herikstad@gmail.com>
2012-03-24 09:41:22 +0800

    Added a class SignalProcessor

---
+++ SignalProcessor.h
@@ -0,0 +1,17 @@
+//
+//  SignalProcessor.h
+//  RawDataViewer
+//
+//  Created by Roger Herikstad on 24/3/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SignalProcessor : NSObject {
+    IBOutlet NSView *signalView;
+    
+}
+
+
+@end
---
+++ SignalProcessor.m
@@ -0,0 +1,23 @@
+//
+//  SignalProcessor.m
+//  RawDataViewer
+//
+//  Created by Roger Herikstad on 24/3/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SignalProcessor.h"
+
+@implementation SignalProcessor
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
