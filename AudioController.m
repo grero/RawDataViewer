e570e245557192ad42f7a7e579d0c6bd2c9e58ca
Roger Herikstad <roger.herikstad@gmail.com>
2012-03-24 10:54:03 +0800

    some files were accidentally deleted. This brought them back

---
+++ AudioController.m
@@ -0,0 +1,23 @@
+//
+//  AudioController.m
+//  RawDataViewer
+//
+//  Created by Roger Herikstad on 21/3/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AudioController.h"
+
+@implementation AudioController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
